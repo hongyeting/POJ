76a9f9ff23405e4e194ee2657175c773e15e1578
hongyt <77911615+hongyeting@users.noreply.github.com>
2021-02-23 09:57:22 +0800

    Update 2449 Kth最短路.cpp

--- 2449 Kth最短路.cpp
+++ 2449 Kth最短路.cpp
@@ -10,13 +10,17 @@
 using namespace std;
 
 int N, M, S, T, K;
-int h[1000];//到终点的最短时间
+int nei[1000][2];//构造LINE时用于记录发射邻边/收束邻边
+int t[1000] = { 0 };//求K短路时用于记录访问次数
+int h[1000];//dijkstra时用于记录到终点的最短时间
 bool vis[1000] = { false };//是否遍历
-int t[1000] = { 0 };//访问次数
 
+int lines_size = 0;
 struct LINE {
-	int from, to, time;
-	LINE(int a, int b, int c) :from(a - 1), to(b - 1), time(c) {}
+	int from, to, time, s_to, s_from;
+	LINE(int a, int b, int c) :from(a - 1), to(b - 1), time(c), s_from(nei[from][0]), s_to(nei[to][1]) {
+		nei[from][0] = nei[to][1] = lines_size;lines_size++;
+	}
 };
 vector<LINE>lines;
 
@@ -28,22 +32,12 @@ struct node {
 	}//定义node比较大小的方式，使优先队列里和小的排前面
 };
 
-struct NEI {
-	vector<int>to;
-	vector<int>from;
-	NEI(){}
-};
-vector<NEI>nlines;
-
 int main() {
 	cin >> N >> M;
+	memset(nei, 0x3f, sizeof(nei));
 	int a, b, c;
-	for (int n = 0;n < N;n++)nlines.push_back(NEI());
-	for (int m = 0;m < M;m++) {
+	for (int m = 0;m < M;m++)
 		cin >> a >> b >> c, lines.push_back(LINE(a, b, c));
-		nlines[a - 1].to.push_back(lines.size() - 1);
-		nlines[b - 1].from.push_back(lines.size() - 1);
-	}
 	cin >> S >> T >> K;S--;T--;
 	if (S == T)K++;
 	memset(h, 0x3f, sizeof(h));
@@ -56,12 +50,9 @@ int main() {
 		vis[id] = true;
 		int t = -o.first;
 		h[id] = t;
-		vector<int>& from = nlines[id].from;
-		for (int ix = 0;ix < from.size();ix++) {
-			int i = from[ix];
+		for (int i = nei[id][1];i != INF;i = lines[i].s_to)
 			if (t + lines[i].time < h[lines[i].from])
 				Q.push(make_pair(-t - lines[i].time, lines[i].from));
-		}
 	}//dijkstra完成h[](到终点的预期代价)
 	if (!vis[start])
 		cout << -1 << '\n';//到不了终点
@@ -77,12 +68,9 @@ int main() {
 				break;
 			}
 			if (t[o.id] > maxtime)continue;
-			vector<int>& to = nlines[o.id].to;
-			for (int ix = 0;ix < to.size();ix++) {
-				int i = to[ix];
-				if (lines[i].from == o.id && vis[lines[i].to])
+			for (int i = nei[o.id][0];i != INF;i = lines[i].s_from)
+				if (vis[lines[i].to])
 					Q.push(node(lines[i].to, o.f + lines[i].time));
-			}
 		}
 		if (!find)
 			cout << -1 << '\n';//到达终点的走法小于maxtime种
