d5e73ea6ba47a54c933c41ff56dc3a321ee96194
hongyt <77911615+hongyeting@users.noreply.github.com>
2021-02-26 11:23:35 +0800

    Update 3463 最短路+次短路 dp.cpp

--- 3463 最短路+次短路 dp.cpp
+++ 3463 最短路+次短路 dp.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int S, F, N, M;
 int head[1000];
-int dist[1000][2];
+int dist[1000];
 int ans[1000][2];
 bool vis[1000][2];
 
@@ -40,40 +40,39 @@ int main() {
 		for (int m = 0;m < M;m++)
 			cin >> a >> b >> c, edges.push_back(EDGE(a, b, c));
         cin >> S >> F;S--, F--;
-        ans[S][0] = 1, dist[S][0] = 0;
+        ans[S][0] = 1, dist[S] = 0;
         priority_queue<NODE>Q;
         Q.push(NODE(0, S, 0));
         while (!Q.empty()) {
             NODE q = Q.top();Q.pop();
             if (vis[q.id][q.order])continue;
             vis[q.id][q.order] = true;
+            if (q.dis > dist[F] + 1) {
+                if (q.id == F)break;
+                else continue;
+            }
             for (int i = head[q.id];i != INF;i = edges[i].nxt) {
                 int dis = q.dis + edges[i].dis;
                 int& from = q.id, & to = edges[i].to;
-                if (dis < dist[to][0]) {//发现更短路，更新最短路长度、数目，次短路长度、数目
-                    if (dist[to][0] != INF) {
-                        dist[to][1] = dist[to][0];
+                if (dis < dist[to]) {//发现更短路，更新最短路长度、数目，次短路数目
+                    if (dist[to] == dis + 1) {
                         ans[to][1] = ans[to][0];
-                        Q.push(NODE(dist[to][1], to, 1));//次短路
+                        Q.push(NODE(dis + 1, to, 1));//次短路
                     }
-                    dist[to][0] = dis;
+                    else ans[to][1] = 0;
+                    dist[to] = dis;
                     ans[to][0] = ans[from][0];
                     Q.push(NODE(dis, to, 0));//最短路
                 }
-                else if (dis == dist[to][0])//等于最短路，更新最短路次数
+                else if (dis == dist[to])//等于最短路，更新最短路次数
                     ans[to][0] += ans[from][0];
-                else if (dis < dist[to][1]) {//发现更短的次短路，更新次短路长度、数目
-                    dist[to][1] = dis;
-                    ans[to][1] = ans[from][q.order];
-                    Q.push(NODE(dis, to, 1));//次短路
-                }
-                else if (dis == dist[to][1])//等于次短路，更新次短路数目
+                else if (dis == dist[to] + 1) {//等于次短路，更新次短路数目
+                    if (ans[to][1] == 0)Q.push(NODE(dis, to, 1));//次短路
                     ans[to][1] += ans[from][q.order];
+                }
             }
         }
-        if (dist[F][1] == dist[F][0] + 1)
-            cout << ans[F][1] + ans[F][0] << '\n';
-        else cout << ans[F][0] << '\n';
+        cout << ans[F][1] + ans[F][0] << '\n';
 	}
     return 0;
 }
